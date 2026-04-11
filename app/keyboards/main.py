8dcfe872e93971376d33f2a49c1669e6fb383905
Posijour <168458508+Posijour@users.noreply.github.com>
2026-04-11 16:56:15 +0200

    Update main.py

--- app/keyboards/main.py
+++ app/keyboards/main.py
@@ -4,15 +4,15 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 def main_menu_keyboard(active_shift: bool = False) -> ReplyKeyboardMarkup:
     if active_shift:
         keyboard = [
-            [KeyboardButton(text="+ Заказ"), KeyboardButton(text="Конец смены")],
-            [KeyboardButton(text="Моя статистика")],
-            [KeyboardButton(text="Сегодня vs вчера"), KeyboardButton(text="Неделя")],
+            [KeyboardButton(text="+ Porudžbina"), KeyboardButton(text="Završi smenu")],
+            [KeyboardButton(text="Moja statistika")],
+            [KeyboardButton(text="Danas vs juče"), KeyboardButton(text="Nedelja")],
         ]
     else:
         keyboard = [
-            [KeyboardButton(text="Старт смены")],
-            [KeyboardButton(text="Моя статистика")],
-            [KeyboardButton(text="Сегодня vs вчера"), KeyboardButton(text="Неделя")],
+            [KeyboardButton(text="+ Porudžbina"), KeyboardButton(text="Završi smenu")],
+            [KeyboardButton(text="Moja statistika")],
+            [KeyboardButton(text="Danas vs juče"), KeyboardButton(text="Nedelja")],
         ]
 
     return ReplyKeyboardMarkup(
